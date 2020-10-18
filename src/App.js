e833cea02212168d8137970038c4802e7103f67f
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-10-18 11:14:36 -0400

    added nav bar to each page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
 import ContactForm from './components/Contact';
 import Resume from './components/Resume';
 import FooterFun from './components/Footer';
 
 function App() {
+  const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
+
+  const [currentTab, setCurrentTab] =useState(tabs[0]);
+
+  const renderPage = () => {
+    switch (currentTab) {
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Resume':
+        return <Resume />;
+      case 'Contact':
+        return <ContactForm />;
+      default:
+        return <About />
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header>
-       <Nav></Nav>
-      </header>
+    <div>
+      <Nav tabs={tabs} currentTab={currentTab} setCurrentTab={setCurrentTab} />
       <main>
-        <div>
-          <ContactForm></ContactForm>
-          <Resume></Resume>
-        </div>
+        <div>{renderPage()}</div>
       </main>
-      <footer>
-        <FooterFun></FooterFun>
-      </footer>
+      <FooterFun />
     </div>
   );
 }
