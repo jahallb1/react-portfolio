f7b1b4d1fff2f3223e899669c0f8b0f09d141b17
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-11-14 16:52:19 -0500

    aded new layout stuff for portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,34 +3,36 @@ import React from "react";
 function Portfolio() {
   const projects = [
     {
+      name: "Tv-Tracker",
+      image: "",
+      tools: "HTML, CSS, Javascript",
+      link: "https://stark-brushlands-99590.herokuapp.com/",
+      GitHub: "https://github.com/Dannymont94/tv-tracker",
+      
+    },
+    {
       name: "Lyric-Lookup",
-      class: "",
+      image: "",
       tools: "HTML, CSS, Javascript",
       link: "https://mayorgak.github.io/lyric-look-up",
     },
     {
       name: "Password Generator",
-      class: "",
+      image: "",
       tools: "Javascript",
       link: "https://jahallb1.github.io/passwordgenerator/",
     },
     {
       name: "Run Buddy",
-      class: "",
+      image: "",
       tools: "HTML, CSS",
       link: "https://jahallb1.github.io/run-buddy/",
     },
     {
       name: "Readme Generator",
-      class: "",
+      image: "",
       tools: "Node.js, JavaScript",
       link: "https://github.com/jahallb1/readmeGenerator",
-    },
-    {
-      name: "Budget Tracker",
-      class: "",
-      tools: "HTML, CSS, JavaScript, Node, Mongo",
-      link: "https://github.com/jahallb1/budget-tracker",
     },
     {
         name: "Budget Tracker",
