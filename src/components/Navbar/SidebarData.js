3d0ec290c50e50988bde74600927bc76202b7295
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-12-16 23:09:46 -0500

    added new nav bar and created a new router layout system

---
+++ src/components/Navbar/SidebarData.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import * as RiIcons from 'react-icons/ri'; 
+import * as AiIcons from 'react-icons/ai';
+import * as BiIcons from 'react-icons/bi';
+
+export const SidebarData = [
+    {
+       title: 'Home',
+       path: '/',
+       icon: <AiIcons.AiFillHome />,
+       cName: 'nav-text' 
+    },
+    {
+        title: 'Portfolio',
+        path: '/portfolio',
+        icon: <BiIcons.BiBook />,
+        cName: 'nav-text' 
+     },
+     {
+        title: 'Resume',
+        path: '/resume',
+        icon: <BiIcons.BiDetail />,
+        cName: 'nav-text' 
+     },
+     {
+        title: 'Contact',
+        path: '/contact',
+        icon: <RiIcons.RiContactsFill />,
+        cName: 'nav-text' 
+     },
+
+     
+]
