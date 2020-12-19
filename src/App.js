00bb305b3d99aa8c0fe343adf08a768ca00ea3f5
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-12-18 21:10:24 -0500

    fixed small bug

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import './theme.css';
-import './index.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './components/About';
-import Portfolio from './components/Portfolio';
-import ContactForm from './components/Contact';
-import Resume from './components/Resume';
-import FooterFun from './components/Footer';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import "./theme.css";
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/About";
+import Portfolio from "./components/Portfolio";
+import ContactForm from "./components/Contact";
+import Resume from "./components/Resume";
+import FooterFun from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  
-
   return (
     <div>
       <Router>
-      <Navbar  />
-      <Switch>
-      <Route path='/' exact component={Home} />
-          <Route path='/portfolio' component={Portfolio} />
-          <Route path='/resume' component={Resume} />
-          <Route path='/contact' component={ContactForm} />
-      </Switch>
-      <FooterFun />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/contact" component={ContactForm} />
+        </Switch>
+        <FooterFun />
       </Router>
     </div>
   );
